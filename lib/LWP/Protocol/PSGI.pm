454bd322ba5119d52003e03445f6c17105eaabd7
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-05-10 22:52:11 -0700

    initial commit

---
+++ lib/LWP/Protocol/PSGI.pm
@@ -0,0 +1,139 @@
+package LWP::Protocol::PSGI;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+use parent qw(LWP::Protocol);
+use HTTP::Message::PSGI qw( req_to_psgi res_from_psgi );
+use Guard;
+
+my @protocols = qw( http https );
+my %orig;
+
+my $app;
+
+sub register {
+    my $class = shift;
+    $app = shift;
+
+    for my $proto (@protocols) {
+        $orig{$proto} ||= LWP::Protocol::implementor($proto);
+        LWP::Protocol::implementor($proto, $class);
+    }
+
+    if (defined wantarray) {
+        return guard { $class->unregister };
+    }
+}
+
+sub unregister {
+    my $class = shift;
+    for my $proto (@protocols) {
+        LWP::Protocol::implementor($proto, $orig{$proto});
+    }
+}
+
+sub request {
+    my($self, $request) = @_;
+    my $env = req_to_psgi $request;
+    res_from_psgi $app->($env);
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+LWP::Protocol::PSGI - Override HTTP/HTTPS backend with your own PSGI applciation
+
+=head1 SYNOPSIS
+
+  use LWP::UserAgent;
+  use LWP::Protocol::PSGI;
+
+  # can be Mojolicious, Catalyst ... any PSGI application
+  my $psgi_app = do {
+      use Dancer;
+      setting apphandler => 'PSGI';
+      get '/search' => sub {
+          return 'googling ' . params->{q};
+      };
+      dance;
+  };
+
+  LWP::Protocol::PSGI->register($psgi_app);
+
+  # can hijack any code that uses LWP::UserAgent underneath
+  my $ua  = LWP::UserAgent->new;
+  my $res = $ua->get("http://www.google.com/search?q=bar");
+  print $res->content; # "googling bar"
+
+=head1 DESCRIPTION
+
+LWP::Protocol::PSGI is a module to hijack I<ANY> code that uses
+L<LWP::UserAgent> underneath such that any HTTP or HTTPS requests can
+be routed to your own PSGI application.
+
+Because it works with any code that uses LWP, you can override various
+WWW::/Net::/WebService:: modules such as L<WWW::Mechanize>.
+
+  use WWW::Mechanize;
+  use LWP::Protocol::PSGI;
+
+  LWP::Protocol::PSGI->register($my_psgi_app);
+
+  my $mech = WWW::Mechanize->new;
+  $mech->get("http://amazon.com/"); # $my_psgi_app runs
+
+=head1 METHODS
+
+=over 4
+
+=item register
+
+  LWP::Protocol::PSGI->register($app);
+  my $guard = LWP::Protocol::PSGI->register($app);
+
+Registers an override hook to hijack HTTP requests. If called in a
+non-void context, returns a L<Guard> object that automatically resets
+the override when it goes out of context.
+
+  {
+      my $guard = LWP::Protocol::PSGI->register($app);
+      # code that uses LWP
+  }
+
+  # now LWP uses the original HTTP implementations
+
+=item unregister
+
+  LWP::Protocol::PSGI->unregister;
+
+Resets all the overrides for LWP. If you use the guard interface
+described above, it will be automatically called for you.
+
+=back
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011- Tatsuhiko Miyagawa
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Plack::Client>
+
+=cut
---
+++ t/test.t
@@ -0,0 +1,32 @@
+use strict;
+use Test::More;
+use LWP::UserAgent;
+use LWP::Protocol::PSGI;
+
+my $psgi_app = sub {
+    my $env = shift;
+    return [
+        200,
+        [
+            "Content-Type", "text/plain",
+            "X-Foo" => "bar",
+        ],
+        [ "query=$env->{QUERY_STRING}" ],
+    ];
+};
+
+{
+    my $guard = LWP::Protocol::PSGI->register($psgi_app);
+
+    my $ua  = LWP::UserAgent->new;
+    my $res = $ua->get("http://www.google.com/search?q=bar");
+    is $res->content, "query=q=bar";
+    is $res->header('X-Foo'), "bar";
+
+    use LWP::Simple;
+    my $body = get "https://www.google.com/?q=x";
+    is $body, "query=q=x";
+}
+
+done_testing;
+
